7d7c7334304d05c470ae002c2341de9de6815108
MS Kim(tony9402) <tony9402@soongsil.ac.kr>
2020-02-10 12:23:21 +0900

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 curl -fsSL https://raw.githubusercontent.com/tony9402/macbook_setting/master/youtubedownload.bash | pbcopy
-touch ~/Desktop/.youtube-download.bash
-chmod +x ~/Desktop/.youtube-download.bash
-
 pbpaste > ~/Desktop/.youtube-download.bash
-
+chmod +x ~/Desktop/.youtube-download.bash
 echo 'alias ydd="bash ~/Desktop/.youtube-download.bash"' >> ~/.bash_profile
 source ~/.bash_profile
